f906a556adb46396fb619284136e9b08da178dac
Hareem Mohsin <122696027+hareem09@users.noreply.github.com>
2025-05-05 16:19:41 +0500

    Update student.js

--- controller/student.js
+++ controller/student.js
@@ -1,22 +1,22 @@
-const studentModel=require('../model/studentSchema')
+const userModel=require('../model/userSchema')
 
-const addStudent= async (req,res)=>{
+const addUser= async (req,res)=>{
   try{
-    const student=await studentModel.create(req.body)
-    await student.save()
-    res.status(200).json({message:'student is added successfully' ,student})
+    const user=await userModel.create(req.body)
+    await user.save()
+    res.status(200).json({message:'user is added successfully' ,user})
   }
   catch(error){
     res.status(500).json({
-        message:'failed to add student',
+        message:'failed to add user',
         error: error.message
     })
   }
 }
 
-const loginStudent= async (req,res)=>{
+const loginUser= async (req,res)=>{
     try{
-        const login=await studentModel.create(req.body)
+        const login=await userModel.create(req.body)
         await login.save()
         res.status(200).json({message:'login is done successfully' ,login})  
     }
@@ -27,8 +27,61 @@ const loginStudent= async (req,res)=>{
         })
       }
 }
+const getUser=async(req,res)=>{
+    try{
+        const user=await userModel.findById(req.params.id)
+        res.status(200).json({
+        message:'retrieved successfully',
+        user
+    })
+    }
+    catch(err)
+    {
+        res.status(500).json({
+            message:'failed',
+            error:err.message
+        })
+    }
+  }
+  
+  const updateUser=async(req,res)=>{
+    try{
+        const user=await userModel.findByIdAndUpdate(req.params.id,req.body,{new:true})
+        await user.save()
+        res.status(200).json({
+            message:'user updated successfully',
+            user
+        })
+      }
+          catch(err)
+          {
+              res.status(500).json({
+                  message:'failed to update',
+                  error:err.message
+              })
+          }
+      }
+      const deleteUser=async(req,res)=>{
+        try{
+            const user=await userModel.findByIdAndDelete(req.params.id)
+            res.status(200).json({
+                message:'user deleted successfully',
+                student
+            })
+        }
+        catch(err)
+        {
+            res.status(500).json({
+                message:'failed to delete',
+                error:err.message
+            })
+        }
+      }
 
-module.exports={
-    addStudent,
-    loginStudent
+  module.exports={
+    addUser,
+    loginUser,
+    getUser,
+    updateUser,
+    deleteUser
 }
