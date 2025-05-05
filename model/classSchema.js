fc577e38efcce3f7d2ac51c7b2bd45d216188c0d
Hareem Mohsin <122696027+hareem09@users.noreply.github.com>
2025-05-05 15:49:06 +0500

    Update classSchema.js

--- model/classSchema.js
+++ model/classSchema.js
@@ -3,15 +3,18 @@ const mongodb=require('mongoose');
 const classSchema= new mongodb.Schema({
     title:{
         type:String,
-        required: true
+        required: true,
+        timestamp:true
     },
     description:{
         type:[String],
-        required: true
+        required: true,
+        timestamp:true
     },
     quiz:{
         type:[String],
-        required: true
+        required: true,
+        timestamp:true
     }
 })
 module.exports= mongodb.model('Class',classSchema);
