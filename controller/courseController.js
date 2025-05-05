280295ebd3355d382618b1a16f2ab98d83397076
Hareem Mohsin <122696027+hareem09@users.noreply.github.com>
2025-05-05 16:41:16 +0500

    Update and rename course.js to courseController.js

--- controller/courseController.js
+++ controller/courseController.js
@@ -36,7 +36,7 @@ const getAllCourse=async(req,res)=>{
 
 const getCourse=async(req,res)=>{
     try{
-        const course=await courseModel.findById(req.params.id)
+        const course=await courseModel.findById(req.params.id).populate('userId','student')
         res.status(200).json({
             message:'course retrieved successfully',
             course
@@ -50,8 +50,44 @@ const getCourse=async(req,res)=>{
         })
     }
 }
+const updateCourse=async(req,res)=>{
+    try{
+        const course=await courseModel.findByIdAndUpdate(req.params.id,req.body,{new:true})
+        await course.save()
+        res.status(200).json({
+            message:'course updated successfully',
+            course
+        })
+    }
+        catch(err)
+        {
+            res.status(500).json({
+                message:'failed to update',
+                error:err.message
+            })
+        }
+    }
+    const deleteCourse=async(req,res)=>{
+        try{
+            const course=await courseModel.findByIdAndDelete(req.params.id)
+            res.status(200).json({
+                message:'course deleted successfully',
+                course
+            })
+        }
+        catch(err)
+        {
+            res.status(500).json({
+                message:'failed to delete',
+                error:err.message
+            })
+        }
+    }
+        
 module.exports={
     addCourse,
     getAllCourse,
-    getCourse
+    getCourse,
+    updateCourse,
+    deleteCourse
 }
